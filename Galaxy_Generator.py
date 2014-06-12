c056f6a49d8a8e8ce53e0c08138bb9f878d3ea14
Michael Prijatelj <michaelprijatelj@gmail.com>
2014-06-12 13:18:05 -0400

    Rough changes for first round of testing

--- Galaxy_Generator.py
+++ Galaxy_Generator.py
@@ -49,19 +49,18 @@ def main(argv):
     logger.info('Starting part B: chromatic bulge+disk galaxy')
     totalFlux = 4.8
     # Iterations to complete
-    fluxNum = 5
-    redshiftNum = 5
-    noiseIterations = 10
+    fluxNum = 3
+    redshiftNum = 3
+    noiseIterations = 100
     # Other parameters
-    redshiftMin, redshiftMax = 0.0, 1.5
+    fluxMin, fluxMax = 0.0, 1.0
+    redshiftMin, redshiftMax = 0.2, 1.0
     bulgeG1, bulgeG2 = 0.12, 0.07
     diskG1, diskG2 = 0.4, 0.2
     mono_bulge_HLR, mono_disk_HLR = 0.5, 2.0
     psf_FWHM, psf_beta = 0.6, 2.5
     noiseSigma = 0.02
-    # Bulge-to-total ratio can only range from 0 to 1; the amount of bulge flux
-    # cannot be greater than the total amount of flux.
-    for fluxRatio in numpy.linspace(0.0,1.0,fluxNum):   
+    for fluxRatio in numpy.linspace(fluxMin,fluxMax,fluxNum):   
         for redshift in numpy.linspace(redshiftMin,redshiftMax,redshiftNum):
             mono_bulge = galsim.DeVaucouleurs(half_light_radius=mono_bulge_HLR)
             bulge_SED = SEDs['CWW_E_ext'].atRedshift(redshift)
@@ -87,13 +86,19 @@ def main(argv):
                     os.mkdir('output_{}'.format(filter_name))
                 outDir = "output_{}/".format(filter_name)
                 outpath = os.path.abspath(os.path.join(path, outDir))
+                img = galsim.ImageF(64, 64, scale=pixel_scale)
+                bdfinal.drawImage(filter_, image=img)
                 images = []
                 # Create many images with different noises, compile into a cube
                 for i in xrange(noiseIterations):
-                    img = galsim.ImageF(64, 64, scale=pixel_scale)
-                    bdfinal.drawImage(filter_, image=img)
-                    img.addNoise(gaussian_noise)
-                    images.append(img)
+                    newImg = img.copy()
+                    newImg.addNoise(gaussian_noise)
+                    images.append(newImg)
+                totalFlux = 0
+                for image in images:
+                    flux = galsim.hsm.FindAdaptiveMom(image)
+                    totalFlux += flux.moments_amp
+                avgFlux = totalFlux/len(images)
                 logger.debug('Created {}-band image'.format(filter_name))
                 fitsName = 'gal_{}_{}_{}.fits'.format(filter_name,fluxRatio,
                                                       redshift)
@@ -103,7 +108,7 @@ def main(argv):
                              + 'bulge-to-total ratio = {}, '
                              + 'redshift = {}'.format(filter_name, fluxRatio, 
                                                       redshift))
-                logger.info('Added flux for {}-band image: {}'.format(filter_name, img.added_flux))
+                logger.info('Average flux for {}-band image: {}'.format(filter_name, avgFlux))                
 
     logger.info('You can display the output in ds9 with a command line that looks something like:')
     logger.info('ds9 -rgb -blue -scale limits -0.2 0.8 output_r/gal_r.fits -green -scale limits'
