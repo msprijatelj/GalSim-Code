ab6ec05d78be63c2e284714bc351e2e828cb4600
Michael Prijatelj <michaelprijatelj@gmail.com>
2014-06-12 16:12:44 -0400

    Fixed averages problem, added success, sigma, and stdev

--- Galaxy_Generator.py
+++ Galaxy_Generator.py
@@ -1,11 +1,88 @@
 # Taken and modified from demo12.py in Galsim/examples
 import sys
 import os
+import shutil
 import math
 import numpy
 import logging
 import galsim
 
+def main(argv):
+    # Where to find and output data
+    path, filename = os.path.split(__file__)
+    datapath = os.path.abspath(os.path.join(path, "data/"))
+    outpath = os.path.abspath(os.path.join(path, "output/"))
+
+    # In non-script code, use getLogger(__name__) at module scope instead.
+    logging.basicConfig(format="%(message)s", level=logging.INFO, 
+                        stream=sys.stdout)
+    logger = logging.getLogger("demo12")
+
+    # initialize (pseudo-)random number generator
+    random_seed = 1234567
+    rng = galsim.BaseDeviate(random_seed)
+    # read in SEDs
+    SEDs = readInSEDs(datapath)
+    logger.debug('Successfully read in SEDs')
+    # read in the LSST filters
+    filters = readInLSST(datapath)
+    logger.debug('Read in filters')
+
+    pixel_scale = 0.2 # arcseconds
+
+    #-----------------------------------------------------------------------------------------------
+
+    logger.info('')
+    logger.info('Starting to generate chromatic bulge+disk galaxy')
+    # Iterations to complete
+    fluxNum = 3
+    redshiftNum = 3
+    noiseIterations = 100
+    # Other parameters
+    fluxMin, fluxMax = 0.0, 1.0
+    redshiftMin, redshiftMax = 0.2, 1.0
+    noiseSigma = 0.02
+    for fluxRatio in numpy.linspace(fluxMin,fluxMax,fluxNum):   
+        for redshift in numpy.linspace(redshiftMin,redshiftMax,redshiftNum):
+            bulge = makeBulge(redshift, SEDs)      
+            logger.debug('Created bulge component')            
+            disk = makeDisk(redshift, SEDs)
+            logger.debug('Created disk component')
+            bdfinal = makeFinal(fluxRatio, bulge, disk)
+            logger.debug('Created bulge+disk galaxy final profile')
+        
+            # draw profile through LSST filters
+            gaussian_noise = galsim.GaussianNoise(rng, sigma=noiseSigma)
+            for filter_name, filter_ in filters.iteritems():
+                #if filter_name == "u": continue
+                outputDir = 'output_{}'.format(filter_name)
+                if os.path.isdir(outputDir):
+                    shutil.rmtree(outputDir)
+                os.mkdir(outputDir)
+                outDir = "output_{}/".format(filter_name)
+                outpath = os.path.abspath(os.path.join(path, outDir))
+                img = galsim.ImageF(64, 64, scale=pixel_scale)
+                bdfinal.drawImage(filter_, image=img)
+                images = makeCubeImages(img, gaussian_noise, noiseIterations)
+                avgFlux, avgSigma, stDev, successRate = findAvgFlux(images)
+                logger.debug('Created {}-band image'.format(filter_name))
+                fitsName = 'gal_{}_{}_{}.fits'.format(filter_name,fluxRatio,
+                                                      redshift)
+                out_filename = os.path.join(outpath, fitsName)
+                galsim.fits.writeCube(images, out_filename)
+                logger.debug('Wrote {}-band image to disk, '
+                             + 'bulge-to-total ratio = {}, '
+                             + 'redshift = {}'.format(filter_name, fluxRatio, 
+                                                      redshift))
+                logger.info('Average flux for {}-band image: {}'.format(filter_name, avgFlux))
+                logger.info('Average Sigma = {}'.format(avgSigma))                
+                logger.info('Standard Deviation = {}'.format(stDev))                
+                logger.info('Success Rate = {}'.format(successRate))                
+                
+    logger.info('You can display the output in ds9 with a command line that looks something like:')
+    logger.info('ds9 -rgb -blue -scale limits -0.2 0.8 output_r/gal_r.fits -green -scale limits'
+                +' -0.25 1.0 output_i/gal_i.fits -red -scale limits -0.25 1.0 output_z/gal_z.fits'
+                +' -zoom 2 &')
 
 def readInSEDs(datapath):
     SED_names = ['CWW_E_ext', 'CWW_Sbc_ext', 'CWW_Scd_ext', 'CWW_Im_ext']
@@ -53,77 +130,6 @@ def makeFinal(fluxRatio, bulge, disk):
     bdfinal = galsim.Convolve([bdgal, PSF])    
     return bdfinal
 
-def main(argv):
-    # Where to find and output data
-    path, filename = os.path.split(__file__)
-    datapath = os.path.abspath(os.path.join(path, "data/"))
-    outpath = os.path.abspath(os.path.join(path, "output/"))
-
-    # In non-script code, use getLogger(__name__) at module scope instead.
-    logging.basicConfig(format="%(message)s", level=logging.INFO, 
-                        stream=sys.stdout)
-    logger = logging.getLogger("demo12")
-
-    # initialize (pseudo-)random number generator
-    random_seed = 1234567
-    rng = galsim.BaseDeviate(random_seed)
-    # read in SEDs
-    SEDs = readInSEDs(datapath)
-    logger.debug('Successfully read in SEDs')
-    # read in the LSST filters
-    filters = readInLSST(datapath)
-    logger.debug('Read in filters')
-
-    pixel_scale = 0.2 # arcseconds
-
-    #-----------------------------------------------------------------------------------------------
-
-    logger.info('')
-    logger.info('Starting part B: chromatic bulge+disk galaxy')
-    # Iterations to complete
-    fluxNum = 3
-    redshiftNum = 3
-    noiseIterations = 100
-    # Other parameters
-    fluxMin, fluxMax = 0.0, 1.0
-    redshiftMin, redshiftMax = 0.2, 1.0
-    noiseSigma = 0.02
-    for fluxRatio in numpy.linspace(fluxMin,fluxMax,fluxNum):   
-        for redshift in numpy.linspace(redshiftMin,redshiftMax,redshiftNum):
-            bulge = makeBulge(redshift, SEDs)      
-            logger.debug('Created bulge component')            
-            disk = makeDisk(redshift, SEDs)
-            logger.debug('Created disk component')
-            bdfinal = makeFinal(fluxRatio, bulge, disk)
-            logger.debug('Created bulge+disk galaxy final profile')
-        
-            # draw profile through LSST filters
-            gaussian_noise = galsim.GaussianNoise(rng, sigma=noiseSigma)
-            for filter_name, filter_ in filters.iteritems():
-                if not os.path.isdir('output_{}'.format(filter_name)):
-                    os.mkdir('output_{}'.format(filter_name))
-                outDir = "output_{}/".format(filter_name)
-                outpath = os.path.abspath(os.path.join(path, outDir))
-                img = galsim.ImageF(64, 64, scale=pixel_scale)
-                bdfinal.drawImage(filter_, image=img)
-                images = makeCubeImages(img, gaussian_noise, noiseIterations)
-                avgFlux = findFlux(images)
-                logger.debug('Created {}-band image'.format(filter_name))
-                fitsName = 'gal_{}_{}_{}.fits'.format(filter_name,fluxRatio,
-                                                      redshift)
-                out_filename = os.path.join(outpath, fitsName)
-                galsim.fits.writeCube(images, out_filename)
-                logger.debug('Wrote {}-band image to disk, '
-                             + 'bulge-to-total ratio = {}, '
-                             + 'redshift = {}'.format(filter_name, fluxRatio, 
-                                                      redshift))
-                logger.info('Average flux for {}-band image: {}'.format(filter_name, avgFlux))                
-
-    logger.info('You can display the output in ds9 with a command line that looks something like:')
-    logger.info('ds9 -rgb -blue -scale limits -0.2 0.8 output_r/gal_r.fits -green -scale limits'
-                +' -0.25 1.0 output_i/gal_i.fits -red -scale limits -0.25 1.0 output_z/gal_z.fits'
-                +' -zoom 2 &')
-
 def makeCubeImages(img, gaussian_noise, noiseIterations):
     images = []
     # Create many images with different noises, compile into a cube
@@ -133,13 +139,31 @@ def makeCubeImages(img, gaussian_noise, noiseIterations):
         images.append(newImg)
     return images
 
-def findFlux(images):
-    totalFlux = 0
+def findAvgFlux(images):
+    fluxList = []
+    sigmaList = []
+    successes = 0.0
+    totalAttempts = len(images)
     for image in images:
-        flux = galsim.hsm.FindAdaptiveMom(image)
-        totalFlux += flux.moments_amp
-    avgFlux = totalFlux/len(images)
-    return avgFlux
+        try:
+            flux = galsim.hsm.FindAdaptiveMom(image)
+            successes += 1
+        except:
+            flux = galsim.hsm.FindAdaptiveMom(image, strict = False)
+        fluxList.append(flux.moments_amp)
+        sigmaList.append(flux.moments_sigma)
+    avgFlux = sum(fluxList)/totalAttempts
+    avgSigma = sum(sigmaList)/totalAttempts
+    successRate = successes/totalAttempts
+    stDev = calcStDev(fluxList, avgFlux)
+    return avgFlux, avgSigma, stDev, successRate
+
+def calcStDev(entries, mean):
+    devTotal = 0
+    for entry in entries:
+        devTotal += (mean - entry) ** 2
+    stDev = math.sqrt(devTotal/len(entries))
+    return stDev
 
 if __name__ == "__main__":
     main(sys.argv)
